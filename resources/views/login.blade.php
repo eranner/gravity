2bc5b7617152fa706d8c8fe6bd15c67dc9c6a529
Eric Ranner <eranner@gmail.com>
2024-03-13 10:27:58 -0400

    added footer and changed login page to include test credentials

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,6 @@
 @include ('partials.header')
 @include ('partials.navbar')
-<h2 style='text-align:center;' class="login-header">Login to your account</h2>
+<h2 style='text-align:center;' class="login-header">Login to Employee Portal</h2>
 <form action="{{route('loginAction')}}" method="POST" style="max-width: 300px; margin:auto;">
     @csrf
     <label for="email" class="form-label readable">Email</label>
@@ -9,4 +9,24 @@
     <input type="password" name="password" class="form-control form-control-sm">
     <input type="submit" value="Login" class="btn btn-primary mt-3" style="margin:auto;">
 </form>
+<div class="container mt-4" style="max-width: 400px;">
+    <div class="accordion" id="accordionFlushExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne" style="font-weight:bold; text-align:center;">
+              Expand for Demo Login Credentials
+            </button>
+          </h2>
+          <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">
+                <ul>
+                    <li style="list-style-type: none;"><span style="font-weight:bold;">Email:</span> testadmin@icecreamshop.site</li>
+                    <li style="list-style-type: none;"><span style="font-weight:bold;">Password:</span> secret123</li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+
 @include ('partials.footer')
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,16 @@
-
+<div class="container">
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="{{route('main')}}" class="nav-link px-2 text-body-secondary">Home</a></li>
+        <li class="nav-item"><a href="{{route('mobileorders')}}" class="nav-link px-2 text-body-secondary">Mobile Orders</a></li>
+        <li class="nav-item"><a href="{{ auth()->check() ? route('dashboard') : route('login')}}" class="nav-link px-2 text-body-secondary">{{auth()->check() ?  'Inventory' :  'Login'}}</a></li>
+        <li class="nav-item"><a href="{{ auth()->check() ? route('orderFiller') : route('login')}}" class="nav-link px-2 text-body-secondary">{{auth()->check() ?  'Orders' :  ''}}</a></li>
+        @if(auth()->check())
+        <li class="nav-item"><a href="{{route('logout')}}" class="nav-link px-2 text-body-secondary">Logout</a></li>
+        @endif
+      </ul>
+      <p class="text-center text-body-secondary">&copy; <?php echo date('Y');?> <a href="https://eranner.website/ej/">EJ Web Design</p>
+    </footer>
+  </div>
 </body>
 </html>
